c0d48fca2d18b553f7d6a7203975190cd6ad8a82
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-07 23:05:11 +0600

    connect server side with database

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4u6eq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const stockCollection = client.db('bicycleWarehouse').collection('stock');
+
+    app.get('/stock', async (req, res) => {
+      const query = {};
+      const cursor = stockCollection.find(query);
+      const stocks = await cursor.toArray();
+      res.send(stocks);
+    });
+  } finally {
+    // some code that stop this function
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Running warehouse Server');
